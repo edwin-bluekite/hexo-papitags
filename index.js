86b980a26f91edc6bee725574f79d1fa8fa46ca2
Edwin Allen <edwin.allen@xoom.com>
2015-05-25 12:37:59 -0600

    csstag

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 
-hexo.extend.tag.register('papitag', function(args, content){	
-  var className =  args.join(' ');
-  return '<blockquote class="pullquote2' + className + '">' + content + '</blockquote>';
+hexo.extend.tag.register('classtag', function(args, content){	
+	var result = '';
+
+	//    args.unshift('pullquote');
+	result += '<div class="' + args[0] + '">';
+	if(args[1]){
+	    result += '<div class="' + args[1] + '">';
+	}
+	result += ctx.render.renderSync({text: content, engine: 'markdown'});
+	result += '</div>';
+	if(args[1]){
+	    result += '</div>';
+	}
+	return result;    
 }, {ends: true});
