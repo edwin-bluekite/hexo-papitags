f3cbb46f10a4fdd806c6df5ac0d3c43eaa3c26ad
Edwin Allen <edwin.allen@xoom.com>
2015-05-25 14:16:58 -0600

    ussing hexo var as context

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-module.exports = function(ctx){
-  var tag = ctx.extend.tag;
-    tag.register('classtag', function(args, content){	
-	var result = '';
+hexo.extend.tag.register('classtag', function(args, content){	
+    var result = '';
 
-	//    args.unshift('pullquote');
-	result += '<div class="' + args[0] + '">';
-	if(args[1]){
-	    result += '<div class="' + args[1] + '">';
-	}
-	result += ctx.render.renderSync({text: content, engine: 'markdown'});
+    //    args.unshift('pullquote');
+    result += '<div class="' + args[0] + '">';
+    if(args[1]){
+	result += '<div class="' + args[1] + '">';
+    }
+    result += hexo.render.renderSync({text: content, engine: 'markdown'});
+    result += '</div>';
+    if(args[1]){
 	result += '</div>';
-	if(args[1]){
-	    result += '</div>';
-	}
-	return result;    
+    }
+    return result;    
 }, {ends: true});
